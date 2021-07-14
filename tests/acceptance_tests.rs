c2a692f809331e172e4b4c92d927592188755bdc
Pascal Van Cauwenberghe <pvc@nayima.be>
2021-07-14 11:38:20 +0200

    Fix typo

--- tests/acceptance_tests.rs
+++ tests/acceptance_tests.rs
@@ -1,7 +1,7 @@
 use rust_sudoku::sudoku::*;
 
 // Acceptance tests with increasingly difficult Sudokus to solve
-// For now, the test will fail because the game doesn't do anything, but it gives us a a gola to works towards.
+// For now, the test will fail because the game doesn't do anything, but it gives us a a goal to works towards.
 // Once the easy sudoku succeeds we can add gradually more complex Sudokus
 // Examples from https://sandiway.arizona.edu/sudoku/examples.html
 
